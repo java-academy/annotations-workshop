052723ffa1d4b9d72f90258d7824003d8668fe73
Tomasz Seidler <tomasz.z.seidler@gmail.com>
2018-05-18 17:14:01 +0200

    prepared for bean error exploration

---
+++ src/main/java/edu/annotations/AppConfig.java
@@ -0,0 +1,12 @@
+package edu.annotations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean(name = "myBean", initMethod = "creation", destroyMethod = "kill")
+    public HelloWorld getHelloWorld() {
+        return new HelloWorld();
+    }
+}
---
+++ src/main/java/edu/annotations/HelloWorld.java
@@ -0,0 +1,15 @@
+package edu.annotations;
+
+public class HelloWorld {
+    public void sayHello() {
+        System.out.println("Hello world!");
+    }
+
+    private void creation() {
+        System.out.println("after initialization...");
+    }
+
+    private void kill() {
+        System.out.println("killing...");
+    }
+}

0ab1a36d4934316caee6c1bee00b8bf7cb1c73e0
Damian <damian.andrzej.muca@gmail.com>
2018-05-19 16:00:20 +0200

    Task 1 - Create your own testing framework from annotations @MyTest, @MyBeforeTest

---
+++ MyTestingFramework/src/main/java/Tests.java
@@ -0,0 +1,24 @@
+package main.java;
+
+public class Tests
+{
+    @MyBeforeTest
+    public void setUp(){
+        System.out.println("I am invoked before any method with @MyTest annotation.");
+    }
+
+    @MyTest
+    public void test1(){
+        System.out.println("Test numer 1.");
+    }
+
+    @MyTest
+    public void test2(){
+        System.out.println("Test number 2.");
+    }
+
+    @MyTest
+    public void test3(){
+        System.out.println("Test number 3.");
+    }
+}

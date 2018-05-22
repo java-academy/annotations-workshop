80c88ebed3169291d2197bbfaf323427e91fa436
Damian Muca <damian_muca@epam.com>
2018-05-22 09:39:39 +0200

    Move src directory up

--- MyTestingFramework/src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-package main.java;
-
-public class Main {
-    public static void main(String[] args) {
-        TestFramework.runTestsFrom(new Tests());
-    }
-}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,5 @@
 public class Main {
+    public static void main(String[] args) {
+        TestFramework.runTestsFrom(new Tests());
+    }
 }

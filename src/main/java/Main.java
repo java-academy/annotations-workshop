2f4b3258d1c7509c525d8aaf5c48a0eef719ff11
Damian <damian.andrzej.muca@gmail.com>
2018-06-03 22:10:30 +0200

    Remove package

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,3 @@
-package main.java;
-
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
--- src/main/java/SomeMethods.java
+++ src/main/java/SomeMethods.java
@@ -1,5 +1,3 @@
-package main.java;
-
 public class SomeMethods {
 
     @Author("Krzyś")
